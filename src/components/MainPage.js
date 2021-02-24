8d81dcd50a8f059f4b9b13aab327bafcc24b589a
Jasonnnz <jzhou35@buffalo.edu>
2021-02-24 11:40:46 -0500

    Create MainPage.js

---
+++ src/components/MainPage.js
@@ -0,0 +1,12 @@
+import TrelloBoard from './TrelloBoard';
+
+function MainPage(){
+    return (
+        <div>
+            "Hi"
+            {/* <TrelloBoard></TrelloBoard> */}
+        </div>
+    );
+}
+
+export default MainPage;
