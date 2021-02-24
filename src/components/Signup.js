6149f7f2cad72efc4e8d218e541612096ffaed7f
Jasonnnz <jzhou35@buffalo.edu>
2021-02-24 11:40:00 -0500

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,16 +1,31 @@
 import { useState } from 'react';
 
-function Signup({loginForm, setLoginForm}){
+function Signup({loginForm, setLoginForm, setCurrentUser, history}){
     const [ email, setEmail] = useState("")
     const [ name, setName] = useState("")
     const [ password, setPassword] = useState("")
 
+    function handleSignup(e){
+        e.preventDefault()
+        const newUser = {
+            email: email,
+            name: name,
+            password: password
+        }
+        fetch('http://localhost:3000/me')
+        .then(r => r.json())
+        .then(user => {
+            setCurrentUser(user)
+            history.push('/main')
+        })
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSignup}>
             <input type="text" name="name" value={name} onChange={(e)=>setName(e.target.value)} placeholder="Enter Name"/>
             <input type="text" name="email" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder="Enter Email"/>
             <input type="password" name="password" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="Enter Password"/>
-            <input type="submit" value="Login"/>
+            <input type="submit" value="Signup"/>
             <p>Have an account? <span onClick={()=>setLoginForm(!loginForm)}>Login</span></p>
         </form>
     );
