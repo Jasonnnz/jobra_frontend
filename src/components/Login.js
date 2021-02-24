b04c5d61335c7a87ec613d1efb125a9d60ffd128
Jasonnnz <jzhou35@buffalo.edu>
2021-02-24 11:39:32 -0500

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,32 @@
 import { useState } from 'react';
 
-function Login({loginForm, setLoginForm}){
+function Login({loginForm, setLoginForm, setCurrentUser, history}){
     const [ email, setEmail] = useState("")
     const [ password, setPassword] = useState("")
 
+    function handleLogin(e){
+        e.preventDefault()
+        const loginUser = {
+            email: email,
+            password: password
+        }
+        fetch('http://localhost:3000/login', {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(loginUser),
+          })
+        .then(r => r.json())
+        .then(user => {
+            setCurrentUser(user)
+            history.push('/main')
+        })
+
+    }
+
     return (
-        <form>
+        <form onSubmit={handleLogin}>
             <input type="text" name="email" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder="Enter Email"/>
             <input type="password" name="password" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="Enter Password"/>
             <input type="submit" value="Login"/>
