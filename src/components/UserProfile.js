d37e730df9866423d6cf0149b59248152a3f217e
Jason Zhou <jzhou35@buffalo.edu>
2021-03-02 14:48:51 -0500

    Used Chart.js to make pie chart

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,4 +1,19 @@
+import {Pie} from 'react-chartjs-2';
+import {useState} from 'react';
+
 function UserProfile({currentUser}){
+    const [data, setData] = useState({
+        labels: ["Interested","Applied", "Accepted", "Rejected"],
+        datasets: [{
+            data: [currentUser.lanes[0].cards.length,
+                 currentUser.lanes[1].cards.length,
+                 currentUser.lanes[2].cards.length,
+                 currentUser.lanes[3].cards.length],
+            backgroundColor: ["blue","yellow","green","red"]
+        }]
+    })
+    console.log(currentUser)
+
     return (
         <div>
             <div className="current-user-info">
@@ -7,6 +22,12 @@ function UserProfile({currentUser}){
             </div>
             <div className="current-user-charts">
                 Charts
+                <Pie 
+                data={{
+                    labels: data.labels,
+                    datasets: data.datasets
+                }}
+                height='60%'/>
             </div>
         </div>
     );
